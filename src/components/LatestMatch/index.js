56ac0f6beabb10a251c05aba42a85ef2c15eb7b5
kanchu-id12 <165665240+kanchu-id12@users.noreply.github.com>
2024-08-14 14:56:11 +0530

    Update index.js

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,11 +1,12 @@
 import './index.css'
 const LatestMatch = props => {
   const {latestMatchDetails} = props
+  const {competingTeam, competingLogoTeam, id, date} = latestMatchDetails
   return (
     <div className="latest-match-container">
       <h1 className="main-heading">Latest Matches</h1>
       <div className='latest-match'>
-       <h1>Delhi Capitals</h1>
+       <h1>{competingTeam}</h1>
       </div>
     </div>
   )
